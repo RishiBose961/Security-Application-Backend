32c66e80da6eaa1d99ed282727f88115de41e4b2
RishiBose961 <rishibose1901@gmail.com>
2024-11-05 22:41:51 +0530

    first commit

--- client/src/pages/Auth/QRCodeLogin.jsx
+++ client/src/pages/Auth/QRCodeLogin.jsx
@@ -12,28 +12,23 @@ const QRCodeLogin = () => {
   // Fetch QR Code on component mount
   useEffect(() => {
     const fetchQRCode = async () => {
-      if (!userInfo?._id) {
-        console.error("User information not found.");
-        return;
-      }
-
       try {
-        const response = await axios.get(`/api/auth/generate-qr/${userInfo._id}`);
+        const response = await axios.get(`/api/auth/generate-qr/${userInfo?._id}`);
         setQrCode(response.data.qrCode);
       } catch (error) {
         console.error('Error fetching QR code:', error);
       }
     };
-
     fetchQRCode();
-  }, [userInfo?._id]); // Add userInfo?._id as a dependency
+  }, []);
 
 //   const handleQRCodeScan = async (sessionToken) => {
 //     try {
 //       const response = await axios.post('http://localhost:3000/qr-login', { sessionToken });
 //       console.log(response.data);
+      
+//       setAuthToken(response.data); // Save the auth token
 
-//       setAuthToken(response.data.authToken); // Save the auth token
 //       alert("Login Successful!");
 //     } catch (error) {
 //       console.error('QR code login failed:', error);
@@ -41,10 +36,13 @@ const QRCodeLogin = () => {
 //     }
 //   };
 
+//   console.log(authToken);
+  
+
   return (
     <div>
       <h2>Scan QR Code to Login</h2>
-
+      {/* <button onClick={handleQRCodeScan}>Scan QR Code</button> */}
       {qrCode ? (
         <img src={qrCode} alt="QR Code for Login" />
       ) : (
